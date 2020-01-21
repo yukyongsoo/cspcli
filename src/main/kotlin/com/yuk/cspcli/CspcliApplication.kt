0dfaa97e2810bbf478631da1da23fe4b22ee44af
yongsoo <yozx9079@naver.com>
2020-01-21 23:19:44 +0900

    Initial commit

---
+++ src/main/kotlin/com/yuk/cspcli/CspcliApplication.kt
@@ -0,0 +1,11 @@
+package com.yuk.cspcli
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CspcliApplication
+
+fun main(args: Array<String>) {
+    runApplication<CspcliApplication>(*args)
+}
---
+++ src/test/kotlin/com/yuk/cspcli/CspcliApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yuk.cspcli
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CspcliApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
